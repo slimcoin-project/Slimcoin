dc733b992aeba99c7deb72d7d2b8d03baef6d6dc
Graham Higgins <gjh-github@bel-epa.com>
2017-03-21 04:29:44 +0000

    Prevent invalid memory access on crafted signature Fix transaction list bug.
    (cherry picked from commit f94bfcc844747cb725f9736ff1b8b8562b137903)

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -52,49 +52,35 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet *
     }
     else if(nNet > 0 || wtx.IsCoinBase())
     {
-      //
-      // Credit
-      //
-      BOOST_FOREACH(const CTxOut& txout, wtx.vout)
-        {
-          TransactionRecord sub(hash, nTime);
-                CTxDestination address;
-          sub.idx = parts.size(); // sequence number
-          sub.credit = txout.nValue;
-                if (wtx.IsCoinBase())
+            //
+            // Credit
+            //
+            BOOST_FOREACH(const CTxOut& txout, wtx.vout)
+            {
+                if(wallet->IsMine(txout))
                 {
-                    // Generated
-                    sub.type = TransactionRecord::Generated;
-                }
-                else if (ExtractDestination(txout.scriptPubKey, address) && IsMine(*wallet, address))
-          {
-            // Received by Bitcoin Address
-            sub.type = TransactionRecord::RecvWithAddress;
-            sub.address = CBitcoinAddress(address).ToString();
-          }
-          else
-          {
                     TransactionRecord sub(hash, nTime);
-                    CBitcoinAddress address;
+                    CTxDestination address;
                     sub.idx = parts.size(); // sequence number
                     sub.credit = txout.nValue;
-                if (wtx.IsCoinBase())
-                {
-                    // Generated
-                    sub.type = TransactionRecord::Generated;
+
+                    if (ExtractDestination(txout.scriptPubKey, address) && IsMine(*wallet, address))
+                    {
+                        // Received by Bitcoin Address
+                        sub.type = TransactionRecord::RecvWithAddress;
+                        sub.address = CBitcoinAddress(address).ToString();
                     }
-                    // else if (ExtractAddress(txout.scriptPubKey, address) && wallet->HaveKey(address))
-                    // {
-                    //     // Received by Bitcoin Address
-                    //     sub.type = TransactionRecord::RecvWithAddress;
-                    //     sub.address = address.ToString();
-                    // }
                     else
                     {
                         // Received by IP connection (deprecated features), or a multisignature or other non-simple transaction
                         sub.type = TransactionRecord::RecvFromOther;
                         sub.address = mapValue["from"];
-          }
+                    }
+                    if (wtx.IsCoinBase())
+                    {
+                        // Generated
+                        sub.type = TransactionRecord::Generated;
+                    }
 
           parts.append(sub);
         }
