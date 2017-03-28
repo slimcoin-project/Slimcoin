4218e2071853c2dbd82d15dafd7b71344aed7a8b
Graham Higgins <gjh-github@bel-epa.com>
2017-03-28 19:45:05 +0100

    https://github.com/PIVX-Project/PIVX/commit/62b2b5c082108e117b9300a4664a8ee12e5b1736
    Do not use boost copy_file if Boost < 1.58 (presstab)"

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -7,6 +7,7 @@
 
 #include "walletdb.h"
 #include "wallet.h"
+#include <fstream>
 #include <boost/filesystem.hpp>
 
 using namespace std;
@@ -443,10 +444,12 @@ bool BackupWallet(const CWallet& wallet, const string& strDest)
           pathDest /= wallet.strWalletFile;
 
         try {
-#if BOOST_VERSION >= 104000
+#if BOOST_VERSION >= 158000
           filesystem::copy_file(pathSrc, pathDest, filesystem::copy_option::overwrite_if_exists);
 #else
-          filesystem::copy_file(pathSrc, pathDest);
+          std::ifstream  src(pathSrc.string(), std::ios::binary);
+          std::ofstream  dst(pathDest.string(),   std::ios::binary);
+          dst << src.rdbuf();
 #endif
           printf("copied wallet.dat to %s\n", pathDest.string().c_str());
           return true;
