6c668087c530785b30add82722efa4cd46b029c9
Graham Higgins <gjh@bel-epa.com>
2019-04-20 15:53:05 +0100

    gardening

--- src/qt/inscriptionpage.cpp
+++ src/qt/inscriptionpage.cpp
@@ -31,85 +31,9 @@ using namespace json_spirit;
 
 int getInscriptionBlockCount()
 {
-    /*
-    QSqlQuery query;
-    query.exec(QString("select blockindex from blockindex"));
-    if (query.next())
-    {
-        return query.value(0).toInt();
-    }
-    */
     return 0;
 }
 
-void updateInscription(int index)
-{
-//    try{
-//        QString s = QString::number(index, 10);
-//        std::vector<std::string> args;
-//        args.push_back("getblockhash");
-//        args.push_back(s.toStdString());
-//        Value result = tableRPC.execute(
-//            args[0],
-//            RPCConvertValues(
-//               args[0],
-//               std::vector<std::string>(args.begin() + 1, args.end())));
-//        if (result.type() == str_type)
-//        {
-//            args.clear();
-//            args.push_back("getblock");
-//            args.push_back(result.get_str());
-//            result = tableRPC.execute(
-//                args[0],
-//                RPCConvertValues(
-//                    args[0],
-//                    std::vector<std::string>(args.begin() + 1, args.end())));
-//            if (result.type() == obj_type)
-//            {
-//                Object reply = result.get_obj();
-//                Array  txid  = find_value(reply, "tx").get_array();
-//                BOOST_FOREACH(Value v, txid)
-//                {
-
-//                    args.clear();
-//                    args.push_back("getinscription");
-//                    args.push_back(v.get_str());
-//                    Value value = tableRPC.execute(
-//                       args[0],
-//                       RPCConvertValues(
-//                       args[0],
-//                       std::vector<std::string>(args.begin() + 1, args.end())));
-//                    if (value.type() == obj_type)
-//                    {
-//                        Object reply = value.get_obj();
-//                        /*
-//                        QSqlQuery query;
-//                        query.exec(QString("insert into inscription values('%1','%2',%3)").arg(QString::fromStdString(find_value(reply, "title").get_str())).arg(QString::fromStdString(v.get_str())).arg(index));
-//                        */
-//                    }
-//                }
-//            }
-//        }
-//        /*
-//        QSqlQuery query;
-//        query.exec(QString("UPDATE blockindex set blockindex = %1").arg(index));
-//        */
-//    }catch (json_spirit::Object& objError)
-//    {
-//        throw std::runtime_error("updateInscription error");
-//    }
-//    catch(std::runtime_error& e)
-//    {
-//          throw std::runtime_error("updateInscription error");
-//    }
-//    catch (std::exception& e)
-//    {
-//        throw std::runtime_error("updateInscription error");
-//    }
-}
-
-
-#include "inscriptionpage.moc"
 
 InscriptionPage::InscriptionPage(QWidget *parent) :
     QDialog(parent),
